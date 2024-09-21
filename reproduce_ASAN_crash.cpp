cf92f9f54412afce70424091fb687d6b788440ae
smatchcube <19359975+smatchcube@users.noreply.github.com>
2024-09-21 17:12:45 +0200

    basic MPSolver example

--- reproduce_ASAN_crash.cpp
+++ reproduce_ASAN_crash.cpp
@@ -1,39 +1,58 @@
 #include <iostream>
-#include <vector>
+#include <memory>
 
-#include "ortools/sat/cp_model.h"
-#include "ortools/sat/cp_model_checker.h"
+#include "ortools/linear_solver/linear_solver.h"
 
-using namespace operations_research;
-using namespace operations_research::sat;
+namespace operations_research {
+void LinearProgrammingExample() {
+  std::unique_ptr<MPSolver> solver(MPSolver::CreateSolver("SCIP"));
+  if (!solver) {
+    LOG(WARNING) << "SCIP solver unavailable.";
+    return;
+  }
 
-int main() {
-    std::vector<int> w = {3, 4, 5, 5};
-    std::vector<int> c = {9, 9};
+  const double infinity = solver->infinity();
+  // x and y are non-negative variables.
+  MPVariable* const x = solver->MakeNumVar(0.0, infinity, "x");
+  MPVariable* const y = solver->MakeNumVar(0.0, infinity, "y");
+  LOG(INFO) << "Number of variables = " << solver->NumVariables();
 
-    sat::CpModelBuilder builder;
-    std::vector<std::vector<BoolVar>> assignment(w.size());
-    for (size_t i = 0; i < w.size(); i++) {
-        assignment[i].reserve(c.size());
-        for (size_t j = 0; j < c.size(); j++) {
-            assignment[i].emplace_back(builder.NewBoolVar());
-        }
-        builder.AddExactlyOne(assignment[i]);
-    }
-    for (size_t j = 0; j < c.size(); j++) {
-        LinearExpr expr;
-        for (size_t i = 0; i < w.size(); i++) {
-            expr += LinearExpr::Term(assignment[i][j], w[i]);
-        }
-        builder.AddLinearConstraint(expr, Domain(0, c[j]));
-    }
-    sat::Model model;
-    sat::SatParameters parameters;
-    parameters.set_num_search_workers(4);
-    parameters.set_max_time_in_seconds(10.0);
-    model.Add(sat::NewSatParameters(parameters));
-    auto response = sat::SolveCpModel(builder.Build(), &model);
-    if (response.status() == sat::CpSolverStatus::OPTIMAL || response.status() == sat::CpSolverStatus::FEASIBLE) {
-        std::cout << "all ok";
-    }
+  // x + 2*y <= 14.
+  MPConstraint* const c0 = solver->MakeRowConstraint(-infinity, 14.0);
+  c0->SetCoefficient(x, 1);
+  c0->SetCoefficient(y, 2);
+
+  // 3*x - y >= 0.
+  MPConstraint* const c1 = solver->MakeRowConstraint(0.0, infinity);
+  c1->SetCoefficient(x, 3);
+  c1->SetCoefficient(y, -1);
+
+  // x - y <= 2.
+  MPConstraint* const c2 = solver->MakeRowConstraint(-infinity, 2.0);
+  c2->SetCoefficient(x, 1);
+  c2->SetCoefficient(y, -1);
+  LOG(INFO) << "Number of constraints = " << solver->NumConstraints();
+
+  // Objective function: 3x + 4y.
+  MPObjective* const objective = solver->MutableObjective();
+  objective->SetCoefficient(x, 3);
+  objective->SetCoefficient(y, 4);
+  objective->SetMaximization();
+
+  const MPSolver::ResultStatus result_status = solver->Solve();
+  // Check that the problem has an optimal solution.
+  if (result_status != MPSolver::OPTIMAL) {
+    LOG(FATAL) << "The problem does not have an optimal solution!";
+  }
+
+  LOG(INFO) << "Solution:";
+  LOG(INFO) << "Optimal objective value = " << objective->Value();
+  LOG(INFO) << x->name() << " = " << x->solution_value();
+  LOG(INFO) << y->name() << " = " << y->solution_value();
+}
+}  // namespace operations_research
+
+int main(int argc, char** argv) {
+  operations_research::LinearProgrammingExample();
+  return EXIT_SUCCESS;
 }
